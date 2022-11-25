08fe299ef18304cda723de5a418ef4789fc2ecfa
gustbl4seck <blaseckgustavo@gmail.com>
2022-11-24 21:47:27 -0300

    projeto RG.Electro rev.8

--- js/map.js
+++ js/map.js
@@ -1,16 +1,18 @@
 jQuery(function ($) {
     function init_map1() {
-       var myLocation = new google.maps.LatLng(-23.486512,-47.448846,3);
-       var mapOptions = {    
-           center: myLocation,
-           zoom: 16
-};
-var marker = new google.maps.Marker({
-    position: myLocation,
-    title: "localização"
-});
-var map = new google.maps.Map(document.getElementById("map1"),mapOptions);
-marker.setMap(map);
-}
-init_map1();
+        var myLocation = new google.maps.LatLng(-23.486492304726553, -47.44885673077066,15);
+        var mapOpition = {
+            center: myLocation,
+            zoom: 16
+
+        };
+        var marker = new google.maps.Marker({
+            position: myLocation,
+            title: "Localização"
+        });
+        var map = new google.maps.Map(document.getElementById("map1"),
+            mapOpition);
+        marker.setMap(map);    
+    }
+    init_map1();
 });
